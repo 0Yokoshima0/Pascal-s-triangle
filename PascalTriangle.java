b9f004f0900c6818a3d3f0a83f18ec5484ca0965
Yokoshima <112453676+0Yokoshima0@users.noreply.github.com>
2022-10-18 19:24:20 +0800

    Update PascalTriangle.java

--- PascalTriangle.java
+++ PascalTriangle.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PascalTriangle {
+public class PascalTriangle1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
